95a5c05e75e811ccb807ae48e1ce7f24d234d3f5
Numan Abubakar <bhalonoman@gmail.com>
2023-09-05 00:27:55 +0500

    remove navbar

--- app/(components)/Footer/footer.tsx
+++ app/(components)/Footer/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <p>All right reserved copyright &copy;  2023 by Numan Abubakar</p>
+        <p>All right reserved copyright &copy;  2023 by Numan Abubakar &heart;</p>
     </div>
   )
 }
