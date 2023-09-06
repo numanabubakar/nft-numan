7f84cfde5b0cbe8d1064dd7febf7948ed2eabdf7
Numan Abubakar <bhalonoman@gmail.com>
2023-09-06 23:11:09 +0500

    btn

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ function Home() {
     <p className='text-3xl font-bold'>Discover More NFTs</p>
     <p className='font-light'>Explore new trending NFTs.</p>
   </div>
-  <div className=''>
+  <div className='text-end '>
 <Button title='see all' iconComp={<AiOutlineEye />} />
   </div>
 </div>
